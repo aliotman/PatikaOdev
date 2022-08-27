a4bf814a910724303ba03e158d92af37ff71ca0c
Ali Otman <aliotmangitar@hotmail.com>
2022-08-27 18:57:31 +0300

    Odev1-Soru1 cevap yuklendi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        System.Console.Write("Dizinin eleman sayısını girin: ");
+        tekrargiris:
+        n=int.Parse(Console.ReadLine());
+        while(n<=0)
+        {
+            System.Console.WriteLine("Lütfen Pozitif Bir sayı girin!");
+            goto tekrargiris;
+        }
+        int [] dizi=new int[n];
+        for(int i=0;i<n;i++)
+        {
+          
+            System.Console.Write("{0}. sayıyı girin: ",i+1);
+             dizi[i]= int.Parse(Console.ReadLine());
+        }
+        foreach (var sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+}
